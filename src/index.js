41a0529076074fe0a05be9611223940845b26657
Subhajit Samanta <subhajitsamanta2705@gmail.com>
2025-09-06 10:14:09 +0530

    Updated range of i

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log("index.js");
 console.log("Version 3");
 let i
-for(i=1; i<=10; i++){
+for(i=1; i<=5; i++){
     console.log("Number:", i)
 }
 console.log("for loop executed successfully!");
